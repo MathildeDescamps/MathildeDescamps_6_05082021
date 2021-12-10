97e4ef21f7a92dbfd981885cb1744b3114b72768
Mathilde Descamps <mdescamps@kira-factory.com>
2021-12-10 14:25:01 +0100

    Updated index js for github page.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './main.scss';
 import App from './components/App.js';
 
 ReactDOM.render(
   <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
